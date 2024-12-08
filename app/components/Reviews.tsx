88360bc2233cf3162f9fee0f89504f14d7ed1d1b
Peter Omotosho <omotoshopeter737@gmail.com>
2024-12-08 19:51:38 +0100

    remove unnecessary import

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { ReviewDetails } from '@/constants'
 import Image from 'next/image'
-import React, { useState } from 'react'
 
 interface ReviewIndexProps{
     currentIndex: number
